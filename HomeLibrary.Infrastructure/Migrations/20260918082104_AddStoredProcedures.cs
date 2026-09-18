9afe71a753c631546824377ca2a6bf95a63fbadc
a.trezubov <a.trezubov@gmail.com>
2026-09-18 11:45:32 +0300

    Fix book creation: switch to SQL Server SPs

---
+++ HomeLibrary.Infrastructure/Migrations/20260918082104_AddStoredProcedures.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeLibrary.Infrastructure.Migrations
+{
+    public partial class AddStoredProcedures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // 1. Получение пагинированного списка
+            migrationBuilder.Sql(@"
+                CREATE PROCEDURE [dbo].[sp_GetPagedBooks]
+                    @PageNumber INT,
+                    @PageSize INT
+                AS
+                BEGIN
+                    SET NOCOUNT ON;
+                    IF @PageNumber < 1 SET @PageNumber = 1;
+                    IF @PageSize < 1 SET @PageSize = 10;
+
+                    SELECT [Id], [Title], [Author], [PublishingYear], [TableOfContentsXml]
+                    FROM [dbo].[Books]
+                    ORDER BY [Title]
+                    OFFSET (@PageNumber - 1) * @PageSize ROWS
+                    FETCH NEXT @PageSize ROWS ONLY;
+                END;");
+
+            // 2. Создание книги
+            migrationBuilder.Sql(@"
+                CREATE PROCEDURE [dbo].[sp_InsertBook]
+                    @Title NVARCHAR(250),
+                    @Author NVARCHAR(150),
+                    @PublishingYear INT,
+                    @TableOfContentsXml XML,
+                    @Id INT OUTPUT -- Переводим на OUTPUT
+                AS
+                BEGIN
+                    SET NOCOUNT ON;
+
+                    INSERT INTO [dbo].[Books] ([Title], [Author], [PublishingYear], [TableOfContentsXml])
+                    VALUES (@Title, @Author, @PublishingYear, @TableOfContentsXml);
+
+                    -- Записываем сгенерированный ID в выходной параметр
+                    SET @Id = SCOPE_IDENTITY();
+                END;
+                ");
+
+            // 3. Редактирование книги
+            migrationBuilder.Sql(@"
+                CREATE PROCEDURE [dbo].[sp_UpdateBook]
+                    @Id INT,
+                    @Title NVARCHAR(250),
+                    @Author NVARCHAR(150),
+                    @PublishingYear INT,
+                    @TableOfContentsXml XML
+                AS
+                BEGIN
+                    SET NOCOUNT ON;
+
+                    UPDATE [dbo].[Books]
+                    SET [Title] = @Title,
+                        [Author] = @Author,
+                        [PublishingYear] = @PublishingYear,
+                        [TableOfContentsXml] = @TableOfContentsXml
+                    WHERE [Id] = @Id;
+                END;");
+
+            // 4. Удаление книги
+            migrationBuilder.Sql(@"
+                CREATE PROCEDURE [dbo].[sp_DeleteBook]
+                    @Id INT
+                AS
+                BEGIN
+                    SET NOCOUNT ON;
+
+                    DELETE FROM [dbo].[Books]
+                    WHERE [Id] = @Id;
+                END;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS [dbo].[sp_GetPagedBooks];");
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS [dbo].[sp_InsertBook];");
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS [dbo].[sp_UpdateBook];");
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS [dbo].[sp_DeleteBook];");
+        }
+    }
+}
--- HomeLibrary.Infrastructure/Repositories/BookRepository.cs
+++ HomeLibrary.Infrastructure/Repositories/BookRepository.cs
@@ -1,7 +1,85 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.EntityFrameworkCore;
+//using HomeLibrary.Domain.Entities;
+//using HomeLibrary.Domain.Repositories;
+//using HomeLibrary.Infrastructure.Data;
+
+//namespace HomeLibrary.Infrastructure.Repositories
+//{
+//    public class BookRepository : IBookRepository
+//    {
+//        private readonly AppDbContext _context;
+
+//        public BookRepository(AppDbContext context)
+//        {
+//            _context = context;
+//        }
+
+//        // Реализация постраничного вывода
+//        public async Task<IEnumerable<Book>> GetPagedAsync(int pageNumber, int pageSize)
+//        {
+//            // Защита от некорректных параметров
+//            if (pageNumber < 1) pageNumber = 1;
+//            if (pageSize < 1) pageSize = 10;
+
+//            return await _context.Books
+//                .AsNoTracking()
+//                .OrderBy(b => b.Title) // Сортировка обязательна перед использованием Skip/Take
+//                .Skip((pageNumber - 1) * pageSize)
+//                .Take(pageSize)
+//                .ToListAsync();
+//        }
+
+//        // Получение общего количества для пагинатора на фронтенде
+//        public async Task<int> GetTotalCountAsync()
+//        {
+//            return await _context.Books.CountAsync();
+//        }
+
+//        public async Task<Book?> GetByIdAsync(int id)
+//        {
+//            return await _context.Books.FindAsync(id);
+//        }
+
+//        public async Task AddAsync(Book book)
+//        {
+//            // если с фронтенда пришел 0, сбрасываем состояние ключа,
+//            // чтобы EF Core не пытался трекать дефолтный ноль как готовый первичный ключ
+//            if (book.Id == 0)
+//            {
+//                // Сообщаем EF Core, что ключ будет сгенерирован СУБД (IDENTITY)
+//                _context.Entry(book).Property(b => b.Id).IsModified = false;
+//            }
+
+//            _context.Books.Add(book);
+
+//            await _context.SaveChangesAsync();
+//        }
+//        public async Task UpdateAsync(Book book)
+//        {
+//            _context.Entry(book).State = EntityState.Modified;
+//            await _context.SaveChangesAsync();
+//        }
+
+//        public async Task DeleteAsync(int id)
+//        {
+//            var book = await _context.Books.FindAsync(id);
+//            if (book != null)
+//            {
+//                _context.Books.Remove(book);
+//                await _context.SaveChangesAsync();
+//            }
+//        }
+//    }
+//}
+
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 using HomeLibrary.Domain.Entities;
 using HomeLibrary.Domain.Repositories;
 using HomeLibrary.Infrastructure.Data;
@@ -17,60 +95,78 @@ namespace HomeLibrary.Infrastructure.Repositories
             _context = context;
         }
 
-        // Реализация постраничного вывода
+        // 1. Получение пагинированного списка через хранимую процедуру
         public async Task<IEnumerable<Book>> GetPagedAsync(int pageNumber, int pageSize)
         {
-            // Защита от некорректных параметров
-            if (pageNumber < 1) pageNumber = 1;
-            if (pageSize < 1) pageSize = 10;
+            var paramPageNumber = new SqlParameter("@PageNumber", SqlDbType.Int) { Value = pageNumber };
+            var paramPageSize = new SqlParameter("@PageSize", SqlDbType.Int) { Value = pageSize };
 
             return await _context.Books
+                .FromSqlRaw("EXEC [dbo].[sp_GetPagedBooks] @PageNumber, @PageSize", paramPageNumber, paramPageSize)
                 .AsNoTracking()
-                .OrderBy(b => b.Title) // Сортировка обязательна перед использованием Skip/Take
-                .Skip((pageNumber - 1) * pageSize)
-                .Take(pageSize)
                 .ToListAsync();
         }
 
-        // Получение общего количества для пагинатора на фронтенде
+        // 2. Получение общего количества
         public async Task<int> GetTotalCountAsync()
         {
             return await _context.Books.CountAsync();
         }
 
+        // 3. Получение книги по ID
         public async Task<Book?> GetByIdAsync(int id)
         {
             return await _context.Books.FindAsync(id);
         }
 
+        // 4. Добавление новой книги с получением сгенерированного ID из процедуры
         public async Task AddAsync(Book book)
         {
-            // если с фронтенда пришел 0, сбрасываем состояние ключа,
-            // чтобы EF Core не пытался трекать дефолтный ноль как готовый первичный ключ
-            if (book.Id == 0)
+            var paramTitle = new SqlParameter("@Title", SqlDbType.NVarChar, 250) { Value = book.Title };
+            var paramAuthor = new SqlParameter("@Author", SqlDbType.NVarChar, 150) { Value = book.Author };
+            var paramPublishingYear = new SqlParameter("@PublishingYear", SqlDbType.Int) { Value = book.PublishingYear };
+            var paramXml = new SqlParameter("@TableOfContentsXml", SqlDbType.Xml) { Value = book.TableOfContentsXml };
+
+            // Создаем выходной параметр для получения сгенерированного ID
+            var paramId = new SqlParameter("@Id", SqlDbType.Int)
             {
-                // Сообщаем EF Core, что ключ будет сгенерирован СУБД (IDENTITY)
-                _context.Entry(book).Property(b => b.Id).IsModified = false;
+                Direction = ParameterDirection.Output
+            };
+
+            await _context.Database.ExecuteSqlRawAsync(
+                "EXEC [dbo].[sp_InsertBook] @Title, @Author, @PublishingYear, @TableOfContentsXml, @Id OUTPUT",
+                paramTitle, paramAuthor, paramPublishingYear, paramXml, paramId);
+
+            // Присваиваем полученное значение свойству Id сущности
+            if (paramId.Value != null && paramId.Value != DBNull.Value)
+            {
+                book.Id = (int)paramId.Value;
             }
-
-            _context.Books.Add(book);
-
-            await _context.SaveChangesAsync();
         }
+
+
+        // 5. Редактирование книги через хранимую процедуру
         public async Task UpdateAsync(Book book)
         {
-            _context.Entry(book).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
+            var paramId = new SqlParameter("@Id", SqlDbType.Int) { Value = book.Id };
+            var paramTitle = new SqlParameter("@Title", SqlDbType.NVarChar, 250) { Value = book.Title };
+            var paramAuthor = new SqlParameter("@Author", SqlDbType.NVarChar, 150) { Value = book.Author };
+            var paramPublishingYear = new SqlParameter("@PublishingYear", SqlDbType.Int) { Value = book.PublishingYear };
+            var paramXml = new SqlParameter("@TableOfContentsXml", SqlDbType.Xml) { Value = book.TableOfContentsXml };
+
+            await _context.Database.ExecuteSqlRawAsync(
+                "EXEC [dbo].[sp_UpdateBook] @Id, @Title, @Author, @PublishingYear, @TableOfContentsXml",
+                paramId, paramTitle, paramAuthor, paramPublishingYear, paramXml);
         }
 
+        // 6. Удаление книги через хранимую процедуру
         public async Task DeleteAsync(int id)
         {
-            var book = await _context.Books.FindAsync(id);
-            if (book != null)
-            {
-                _context.Books.Remove(book);
-                await _context.SaveChangesAsync();
-            }
+            var paramId = new SqlParameter("@Id", SqlDbType.Int) { Value = id };
+
+            await _context.Database.ExecuteSqlRawAsync(
+                "EXEC [dbo].[sp_DeleteBook] @Id",
+                paramId);
         }
     }
 }
